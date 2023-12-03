5487a6870ba47e0bb838e95e84db2723b35a1b05
PhilippNiklasGit <philippd1502@gmail.com>
2023-12-03 20:57:14 +0100

    reworked structure for conciseness

--- day3/src/bin/part1.rs
+++ day3/src/bin/part1.rs
@@ -1,16 +1,17 @@
 use core::cmp;
+
 fn main() {
     let input = include_str!("input");
     println!("{}", part1(input));
 }
 
-fn is_adjacent(num: (usize,usize), size: usize, spec_char: (usize, usize)) -> bool {
-    let (num_y, num_x) = num;
-    let (spec_char_y, spec_char_x) = spec_char;
+fn is_adjacent(num: ShemeString, spec_char: ShemeCharacter) -> bool {
+    let (num_y, num_x) = (num.pos.y,num.pos.x);
+    let (spec_char_y, spec_char_x) = (spec_char.pos.y,spec_char.pos.x);
 
 
     let mut dist = usize::MAX;
-    for num_x in num_x..num_x+size {
+    for num_x in num_x..num_x+num.string.len(){
         let y = num_y.abs_diff(spec_char_y);
         let x = num_x.abs_diff(spec_char_x);
 
@@ -21,52 +22,86 @@ fn is_adjacent(num: (usize,usize), size: usize, spec_char: (usize, usize)) -> bo
     }
     dist<2
 }
+#[derive(Debug, Clone)]
+struct Position {
+    y: usize,
+    x: usize
+}
+impl Position {
+    pub fn new(y: usize, x: usize) -> Self {
+        Self {
+            y,
+            x
+        }
+    }
+}
 
+#[derive(Debug, Clone)]
+struct ShemeCharacter {
+    pos: Position,
+    pub character: char 
+}
+
+impl ShemeCharacter {
+    pub fn new(y: usize,x: usize, character: char) -> Self {
+        Self {
+            pos: Position::new(y,x),
+            character
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ShemeString {
+    pos: Position,
+    pub string: String  
+}
+
+impl ShemeString {
+    pub fn from_character(ch: ShemeCharacter) -> Self {
+        Self {
+            pos: ch.pos,
+            string: String::from(ch.character)
+        }
+    }
+}
 
 fn part1(input: &str) -> usize {
-    let new_chars = input.lines().enumerate().map(|(y, line)|{
-        line.chars()
-            .enumerate()
-            .filter(|(_,character)| character!=&'.')
-            .map(move |(x,character)| {
-            ((y,x),character)
+    let (num_arr,spec_chars):(Vec<ShemeCharacter>,Vec<ShemeCharacter>) = input.lines().enumerate()
+        .map(|(y, line)|{
+            line.chars()
+                .enumerate()
+                .filter(|(_,character)| character!=&'.')
+                .map(move |(x,character)| {
+                    ShemeCharacter::new(y,x,character)
+                })
         })
-    })
     .flatten()
-    .collect::<Vec<((usize,usize),char)>>();
-
-    let spec_chars = new_chars.clone().into_iter()
-                            .filter(|character| !character.1.is_numeric())
-                            .collect::<Vec<((usize,usize),char)>>();
-
-    let num_arr = new_chars.into_iter()
-                            .filter(|character| character.1.is_numeric())
-                            .collect::<Vec<((usize,usize),char)>>();
-
+    .partition(|c| c.character.is_numeric());
+    
     let mut num_iter = num_arr.into_iter().peekable();
-    let mut num_arr: Vec<((usize,usize),String)> = [].to_vec();
+    let mut num_arr: Vec<ShemeString> = [].to_vec();
     let mut last_num:isize = -2;
 
-    while let Some(character) = num_iter.next(){
-        let (_,character_x) = character.0;
-        if character_x as isize==last_num+1 {
+    while let Some(character) = &num_iter.next(){
+        if character.pos.x as isize==last_num+1 {
             let mut last = num_arr.pop().unwrap();
-            last.1.push(character.1);
+            last.string.push(character.character);
             num_arr.push(last);
             last_num+=1;
         } else {
-            num_arr.push((character.0, character.1.to_string()));
-            last_num=character_x as isize;
+            num_arr.push(ShemeString::from_character(character.clone()));
+            last_num=character.pos.x as isize;
         }
     };
 
     num_arr.into_iter()
         .filter(|num| {
             spec_chars.clone().into_iter()
-                .filter(|spec_char| is_adjacent(num.0,num.1.len(), spec_char.0))
+                .filter(|spec_char| is_adjacent(num.clone(), spec_char.clone()))
                 .count()>0
         })
-        .map(|character| character.1.parse::<usize>().unwrap())
+        .map(|num| num.string.parse::<usize>().unwrap())
         .sum::<usize>()
 
 }
